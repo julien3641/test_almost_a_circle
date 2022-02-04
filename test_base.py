f5de68ea2d305513d65138599b591ab95e3adc5e
Elodie Riou <elodie.riou@hotmail.com>
2022-02-04 14:22:20 +0100

    add some tests about positive and negative integers

--- test_base.py
+++ test_base.py
@@ -1,1 +1,31 @@
 #!/usr/bin/python3
+"""
+Add unit test for the class Base
+"""
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+
+
+class TestBase(unittest.TestCase):
+    """
+    This class test the class Base.
+    """
+    def work_tests(self):
+        """
+        Tests about:
+            - positive integers
+            - negative integers
+        """
+        b = Base(1)
+        self.assertEqual(b.id, 1)
+        b = Base(12)
+        self.assertEqual(b.id, 12)
+        b = Base(201)
+        self.assertEqual(b.id, 201)
+
+        b = Base(-1)
+        self.assertEqual(b.id, -1)
+        b = Base(-12)
+        self.assertEqual(b.id, -12)
